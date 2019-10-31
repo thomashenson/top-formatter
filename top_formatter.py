e24306ff5eaf3876c50835016f2c4eb07190af3e
thomashenson <thomasjameshenson@gmail.com>
2019-10-31 16:53:16 +0000

    Added csv functionality
    Implemented a .csv solution, which converts the output into comma seperated values and can be viewed as a table in Excel/Libre Office or similar.
    
    Updated .gitignore to include .hidden folder.

--- top_formatter.py
+++ top_formatter.py
@@ -1,19 +1,44 @@
 import subprocess
+import os
 
 
 def top_formatter(ext):
     """
-    Given user inputted file extension, output mem usage sorted by
-    user to an output file.
+    Given desired file extension, stores top output to file in current directory.
+    Supported file extensions:
+    - "txt"
+    - "csv"
+    - "json" (wip)
     :param ext: Desired extension for output file - Type: String
     :return: Success or error message - Type: String
     """
-    extensions = ["txt", "json", "html"]
+    extensions = ["txt", "csv", "json"]
 
     if ext in extensions:
-        with open("output." + ext, "w") as outfile:
-            subprocess.call("top -b -n 1", shell=True, stdout=outfile)
+        # Global file path
+        file_path = "top." + ext
 
-        return "File successfully written to current directory."
+        if ext == "txt":
+            with open(file_path, "w") as outfile:
+                # Store top output in txt file.
+                subprocess.call("top -b -n 1", shell=True, stdout=outfile)
+
+                return ".txt file successfully written to current directory."
+        elif ext == "csv":
+            with open(file_path, "w") as outfile:
+                # Store top output in csv file.
+                subprocess.call("top -b -n 1", shell=True, stdout=outfile)
+
+                # Remove unwanted values at the top of the output and store in new temporary file.
+                # Effectively prints from line 7 to end of output file.
+                subprocess.call("sed -n '7, $p' " + file_path + " > new." + ext, shell=True)
+
+                # Removes trailing/leading whitespace and replaces everything else with a comma.
+                subprocess.call("sed -n '{s/^ *//;s/ *$//;s/  */,/gp;}' new." + ext + " > " + file_path, shell=True)
+
+                # Remove temp file
+                os.remove("new." + ext)
+
+                return ".csv file successfully written to current directory."
     else:
         return "Invalid file extension!"
