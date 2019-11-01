23dc1d9d822d36332cefa17a8f75742baad1f4a0
thomashenson <thomasjameshenson@gmail.com>
2019-11-01 09:46:05 +0000

    Added json functionality
    Implented json functionality, which creates a csv file and converts it to json using a jq script.
    
    As mentioned above, added a jq script to convert csv to json. Lives in scripts/ sub-directory.

--- top_formatter.py
+++ top_formatter.py
@@ -15,30 +15,40 @@ def top_formatter(ext):
     extensions = ["txt", "csv", "json"]
 
     if ext in extensions:
-        # Global file path
-        file_path = "top." + ext
+        txt_path = "top.txt"
+        csv_path = "top.csv"
+        json_path = "top.json"
 
         if ext == "txt":
-            with open(file_path, "w") as outfile:
+            with open(txt_path, "w") as outfile:
                 # Store top output in txt file.
                 subprocess.call("top -b -n 1", shell=True, stdout=outfile)
 
-                return ".txt file successfully written to current directory."
-        elif ext == "csv":
-            with open(file_path, "w") as outfile:
+            return ".txt file successfully written to current directory."
+        elif ext == "csv" or ext == "json":
+            with open(csv_path, "w") as outfile:
                 # Store top output in csv file.
                 subprocess.call("top -b -n 1", shell=True, stdout=outfile)
 
                 # Remove unwanted values at the top of the output and store in new temporary file.
                 # Effectively prints from line 7 to end of output file.
-                subprocess.call("sed -n '7, $p' " + file_path + " > new." + ext, shell=True)
+                subprocess.call("sed -n '7, $p' " + csv_path + " > new.csv", shell=True)
 
                 # Removes trailing/leading whitespace and replaces everything else with a comma.
-                subprocess.call("sed -n '{s/^ *//;s/ *$//;s/  */,/gp;}' new." + ext + " > " + file_path, shell=True)
+                subprocess.call("sed -n '{s/^ *//;s/ *$//;s/  */,/gp;}' new.csv > " + csv_path, shell=True)
 
                 # Remove temp file
-                os.remove("new." + ext)
+                os.remove("new.csv")
 
-                return ".csv file successfully written to current directory."
+                # Convert to json
+                if ext == "json":
+                    subprocess.call("jq -R -s -f scripts/csv2json.jq " + csv_path + " > " + json_path, shell=True)
+
+                    # Remove donor csv file
+                    os.remove(csv_path)
+
+                    return ".json file successfully written to current directory."
+
+            return ".csv file successfully written to current directory."
     else:
         return "Invalid file extension!"
